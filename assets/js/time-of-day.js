e905be95fd14a30e00b11c6b1f0b9bb6d7023f50
Hamidusman <abdulhamidusman218@gmail.com>
2023-11-14 09:55:31 +0000

    l

---
+++ assets/js/time-of-day.js
@@ -0,0 +1,32 @@
+function displayGreeting() {
+    var currentTime = new Date().getHours();
+    var minutes = new Date().getMinutes()
+    var greeting;
+
+    if (currentTime >= 0 && currentTime < 12) {
+        greeting = "Good Morning! The Time Is " + currentTime + ':' + minutes +'AM';
+    } else if (currentTime >= 12 && currentTime < 18) {
+        greeting = "Good Afternoon! The Time Is " + currentTime + ':' + minutes +'PM';
+    } else {
+        greeting = "Good Evening! The Time Is " + currentTime + ':' + minutes +'PM';
+    }
+
+    // Display the greeting in an element with the id "greeting"
+    document.getElementById("time").innerText = greeting;
+}
+window.onload = displayGreeting;
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+      var dropdown = document.getElementById("myDropdown");
+      if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+      }
+    }
+  }
