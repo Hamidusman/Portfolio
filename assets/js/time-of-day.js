7ed730c1d500c4a76c4e81e38d0ad456cd00748f
hamid <abdulhamidusman218.gmail.com>
2023-11-24 19:49:25 +0000

    interface and color theme overhaul

--- assets/js/time-of-day.js
+++ assets/js/time-of-day.js
@@ -20,17 +20,3 @@ function displayGreeting() {
 }
 window.onload = displayGreeting;
 
-function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-btn')) {
-      var dropdown = document.getElementById("myDropdown");
-      if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-      }
-    }
-  }
