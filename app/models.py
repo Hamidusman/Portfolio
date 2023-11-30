32f869193faaece786dfd1d0776e136c5dcfde8d
Hamidusman <abdulhamidusman218@gmail.com>
2023-11-30 10:57:52 +0000

    Created models for tech stack and projects

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TechStack(models.Model):
+    icon = models.ImageField()
+    title = models.CharField(max_length=15)
+    description = models.CharField(max_length=150)
+
+class Project(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=120)
+    date = models.DateField(auto_now=True)
