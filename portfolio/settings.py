176093baffda3f5a4802aadf33eeb3685929bdcf
Hamidusman <abdulhamidusman218@gmail.com>
2024-05-20 15:51:26 +0100

    cleared blog

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,15 +17,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/n .1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9fb3ttjam!3@6b(+#i3#4ijynx)5m^!*))#6&k69swt^x_d6(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-blush-six-15.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
