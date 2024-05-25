ec53b88241c99b713c7927517d6e49d996eac169
Hamidusman <abdulhamidusman218@gmail.com>
2024-05-25 16:03:32 +0100

    project redesign

--- app/models.py
+++ app/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class TechStack(models.Model):
-    icon = models.ImageField(upload_to='techstack_icons/')
-    title = models.CharField(max_length=40)
-    description = models.CharField(max_length=200)
+class TechStack(models.Model): 
+    title = models.CharField(max_length=40) 
 
     def __str__(self):
         return (self.title)
