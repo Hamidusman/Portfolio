5a2e882449fe4e378dd2c18d0adc3f562964af43
Hamidusman <abdulhamidusman218@gmail.com>
2024-05-28 03:01:45 +0100

    staticfile routing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
  
 STATIC_URL = '/static/' 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [ 
+    ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
